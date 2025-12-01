79055e80691939fa4e9a77b3dbeac683d4a40224
Sagar <sagar@Sagars-MacBook-Air.local>
2025-12-01 14:24:26 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,11 +3,13 @@ import sys
 
 def main():
     # TODO: Uncomment the code below to pass the first stage
-    sys.stdout.write('$ ')
-    command = sys.stdin.readline().strip()
-    if command:
-        sys.stdout.write(f"{command}: command not found")
-    pass
+    while True:
+        sys.stdout.write('$ ')
+        command = sys.stdin.readline().strip()
+        if command:
+            sys.stdout.write(f"{command}: command not found\n")
+            sys.stdout.flush()
+        
 
 
 
